8d47044b09f65906c86b63e5647402133bb91b56
Rafay Ahmed <syedrafayahmeds@gmail.com>
2024-02-18 01:48:53 +0500

    added clicks counting to help user

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const colorsName = ['Red','Blue','Green'];
 const audio = []
 let index = 0;
 const text = document.getElementById('text');
+const clicks = document.getElementById('clicks');
+let clicksNum = 0;
 
 text.addEventListener('click', function colorChange(){
-    console.log(index);
     index = (index+1)%colors.length;
     text.innerHTML = colorsName[index];
+    clicks.innerHTML = ++clicksNum;
     const body = document.getElementById('main_window');
     body.style.backgroundColor = colors[index];
     speakColor(colorsName[index]);
 })
 
 function speakColor(color){
-    
     var audio = null;
     switch(color){
         case 'Red':
@@ -28,7 +29,7 @@ function speakColor(color){
             break;
         default:
     }
-    
+
     if (audio != null)
         audio.play();
 }
